948ae8337288a7c6046ace0763b09c0de395ad1d
Aurodddd <pauldeyramirez712@gmail.com>
2025-06-19 19:42:48 +0800

    add-Landingpage

---
+++ Frontend/src/pages/Landingpage/Landingpage.jsx
@@ -0,0 +1,14 @@
+import Home from "./Lpcomponents/Home.jsx"
+import Navbar from "./Lpcomponents/Navbar.jsx"
+
+
+function Landingpage () {
+    return(
+        <>
+        <Navbar/>
+        <Home/>
+        </>
+    )
+}
+
+export default Landingpage
